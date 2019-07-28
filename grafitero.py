ff3f6eb4381637b0fc35c8e9b6182c8d33d7c9c9
makox <valencaligiuri123@gmail.com>
2019-07-28 14:39:42 -0300

    First Commit

---
+++ grafitero.py
@@ -0,0 +1,83 @@
+import pygame
+from pygame.locals import *
+import random
+
+pygame.init()
+
+ventana = pygame.display.set_mode((800,600))
+VerdeFondo = (80,120,80)
+VerdeObjeto = (30,40,30)
+VerdeObjetoClaro = (50, 60, 50)
+
+# Fuentes
+Fuente_Titulo = pygame.font.Font('Font/PressStart2p.ttf',50)
+Fuente_Menu = pygame.font.Font('Font/PressStart2p.ttf',25)
+Fuente_Texto = pygame.font.Font('Font/PressStart2p.ttf', 15)
+
+# Renders
+Titulo = Fuente_Titulo.render('Classic Snake', True, VerdeObjeto)
+UnJugador = Fuente_Menu.render('Un Jugador', True, VerdeObjeto)
+Creditos = Fuente_Menu.render('Creditos', True, VerdeObjeto)
+InstruccionMenu = Fuente_Texto.render('Presione SPACE para entrar a la opcion', True, VerdeObjetoClaro)
+InstruccionMenu2 = Fuente_Texto.render('Presione X para volver al la menu', True, VerdeObjetoClaro)
+Creditos1 = Fuente_Texto.render('Este juego fue desarrollado por', True, VerdeObjetoClaro)
+Creditos2 = Fuente_Texto.render('Valentin Caligiuri, sin fines de lu-', True, VerdeObjetoClaro)
+Creditos3 = Fuente_Texto.render('crar. Solo para disfrutar...', True, VerdeObjetoClaro)
+Score = Fuente_Texto.render('Score: ', True, VerdeObjeto)
+
+#Snake
+secciones = [(80,80),(80,100), (80,120), (80,140), (80,160), (100,160)]
+
+class grafica(object):
+	def __init__(self):
+		print ('Nuevas graficas creadas')
+		pass
+	def Fondo(self):
+		ventana.fill(VerdeFondo)
+
+	def Titulo(self, seleccion):
+		if seleccion == 0:
+			pygame.draw.rect(ventana, VerdeObjetoClaro, (110,200,300,29))
+		elif seleccion == 1:
+			pygame.draw.rect(ventana, VerdeObjetoClaro, (110,230,300,29))
+		ventana.blit(Titulo,(80,50))
+		ventana.blit(UnJugador, (120,200))
+		ventana.blit(Creditos, (120,230))
+		ventana.blit(InstruccionMenu, (10, 550))
+		pass
+	def Creditos(self):
+		ventana.blit(Creditos, (120, 230))
+		ventana.blit(Creditos1, (120, 260))
+		ventana.blit(Creditos2, (120, 280))
+		ventana.blit(Creditos3, (120, 300))
+		ventana.blit(InstruccionMenu2, (10, 550))
+
+	def DibujarSnake(self, orientacion, Pcomida, puntuacion):
+		for i in secciones:
+			pygame.draw.rect(ventana, VerdeObjeto, (i[0]+1,i[1]+1, 18,18))
+			if secciones.index(i) == len(secciones)-1:
+				if orientacion == 0:
+					secciones.append((i[0]+20,i[1]))
+				elif orientacion == 90:
+					secciones.append((i[0],i[1]-20))
+				elif orientacion == 180:
+					secciones.append((i[0]-20,i[1]))
+				elif orientacion == 270:
+					secciones.append((i[0],i[1]+20))
+				if i != Pcomida:
+					del secciones[0]
+					return (Pcomida, puntuacion)
+				else:
+					puntuacion = puntuacion +1
+					return ((random.randint(0,39)*20, random.randint(0,29)*20),puntuacion)
+				break
+
+
+
+	def Comida(self, Pcomida):
+		pygame.draw.rect(ventana,VerdeObjetoClaro,(Pcomida[0],Pcomida[1], 18, 18))
+
+	def Score(self, puntuacion):
+		ventana.blit(Score,(10,10))
+		Puntos = Fuente_Texto.render(str(puntuacion), True, VerdeObjeto)
+		ventana.blit(Puntos,(150,10))
---
+++ main.py
@@ -0,0 +1,77 @@
+import pygame
+from pygame.locals import *
+import sys
+import grafitero
+import time
+
+pygame.init()
+pygame.display.set_mode((800, 600))
+pygame.display.set_caption('Snake: By MaKoX')
+g = grafitero.grafica()
+
+# Variables
+fase = 0
+seleccionmenu = 0
+orientacion = 0
+Pcomida = (200,200)
+puntuacion = 0
+
+# Ciclo del juego
+
+while True:
+    if fase == 0:
+        g.Fondo()
+        g.Titulo(seleccionmenu)
+    elif fase == 1:
+        g.Fondo()
+        Pcomida, puntuacion = g.DibujarSnake(orientacion,Pcomida,puntuacion)
+        g.Comida(Pcomida)
+        g.Score(puntuacion)
+        pass
+    elif fase == 2:
+        g.Fondo()
+        g.Creditos()
+        pass
+    # Cuerpo de eventos
+    for evento in pygame.event.get():
+        if evento.type == QUIT:
+            pygame.quit()
+            sys.exit()
+            pass
+        elif evento.type == KEYDOWN:
+            if fase == 0:
+                if evento.key == pygame.K_DOWN:
+                    if seleccionmenu < 1:
+                        seleccionmenu += 1
+                    else:
+                        seleccionmenu = 0
+                elif evento.key == pygame.K_UP:
+                    if seleccionmenu > 0:
+                        seleccionmenu -= 1
+                    else:
+                        seleccionmenu = 1
+                elif evento.key == pygame.K_SPACE:
+                    if seleccionmenu == 0:
+                        fase = 1
+                    elif seleccionmenu == 1:
+                        fase = 2
+            elif fase == 1:
+                if evento.key == pygame.K_DOWN and orientacion != 90:
+                    orientacion = 270
+                if evento.key == pygame.K_UP and orientacion != 270:
+                    orientacion = 90
+                if evento.key == pygame.K_LEFT and orientacion != 0:
+                    orientacion = 180
+                if evento.key == pygame.K_RIGHT and orientacion != 180:
+                    orientacion = 0
+            elif fase == 2:
+                if evento.key == pygame.K_x:
+                    fase = 0
+        elif evento.type == KEYUP:
+            pass
+
+    pygame.display.update()
+    time.sleep(0.04)
+    pass
+
+print('Todo funciona correctamente')
